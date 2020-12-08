eb9372e118d6b4094ba62dc045b7166a3f6b6581
Hasan ALTAN <hsnaltan13.com>
2020-12-08 09:46:40 +0300

    Add project files.

---
+++ Part_1_DbFirst/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_1_DbFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NorthwindEntities db = new NorthwindEntities();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Categories.ToList();
+
+        }
+    }
+}
