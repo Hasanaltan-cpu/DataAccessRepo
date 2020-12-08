b5c114cce5fdbe79731e2f8888bbb21243b7db49
Hasan ALTAN <hsnaltan13.com>
2020-12-08 19:35:42 +0300

    Linq Methods and Aggregrate Functions

--- Part_1_DbFirst/Form1.Designer.cs
+++ Part_1_DbFirst/Form1.Designer.cs
@@ -29,6 +29,22 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,23 +56,215 @@
             this.dataGridView1.Size = new System.Drawing.Size(772, 165);
             this.dataGridView1.TabIndex = 0;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(28, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Linq To";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(506, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 25);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Aggregate Functions";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 36);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "To List Function";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 29);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Where Function";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(160, 213);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 36);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "OrderBy /OrderByDescending Function";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(160, 253);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 32);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "GroupBy Function";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(19, 293);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 41);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "First Function";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(160, 293);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 41);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "FirstOrDefault Function";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(303, 213);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(107, 37);
+            this.button7.TabIndex = 19;
+            this.button7.Text = "Find Function";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(303, 252);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(107, 35);
+            this.button8.TabIndex = 20;
+            this.button8.Text = "Take Function";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(303, 299);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(107, 28);
+            this.button9.TabIndex = 21;
+            this.button9.Text = "Skip";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(412, 213);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 34);
+            this.button10.TabIndex = 22;
+            this.button10.Text = "Contains";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(412, 253);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 33);
+            this.button11.TabIndex = 23;
+            this.button11.Text = "Any";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(511, 214);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(112, 47);
+            this.button12.TabIndex = 24;
+            this.button12.Text = "Count";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(511, 267);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(112, 49);
+            this.button13.TabIndex = 25;
+            this.button13.Text = "DateDiff";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(629, 214);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(113, 48);
+            this.button14.TabIndex = 26;
+            this.button14.Text = "Sum";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
     }
 }
 
--- Part_1_DbFirst/Form1.cs
+++ Part_1_DbFirst/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.SqlServer;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,174 @@ namespace Part_1_DbFirst
         }
 
         NorthwindEntities db = new NorthwindEntities();
-
-        private void Form1_Load(object sender, EventArgs e)
+        
+        private void button1_Click_1(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = db.Categories.ToList();
+            //LinkTo() " We use this method for listing all values without any condition.It execute on the Heap Field of RAM.It creates a generic list."
 
+            dataGridView1.DataSource = db.Employees.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Where() "It is worked for getting filter datas from table on the database with condition whic is created by us"
+
+            dataGridView1.DataSource = db.Products.Where(x => x.CategoryID != 3).ToList(); /// ın this row we ask to sql to go to product table of nortwind.db then find categoryid is not 3 finally list to me on the datagridview.
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //OrderBy() "It uses for getting list which is wanted by us in terms of numeric sorting.OrderBy=>Ascending
+            //OrderByDescending()"It uses for getting list which is wanted by us in terms of numeric sorting by descending.OrderByDescending=>Descending
+
+
+            dataGridView1.DataSource = db.Products.OrderByDescending(x => x.UnitsOnOrder).ToList();//In this row we ask from sql that is a sorting list descendingly in  terms of  UnitsOnOrder.
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //GroupBy() "It is used for make a group with condition which is written by us and finally group them automatically.
+
+            dataGridView1.DataSource = db.Orders.GroupBy(x => x.EmployeeID).Select(x => new
+            {
+                Satıspersoneli = x.Key,
+                toplamsatis = x.Sum(e => e.OrderID)
+            }).ToList();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //First() " It is used like a "TOP" logic on the SQL . This method takes us to first index whic was searched by us.
+            Product product= db.Products.First();
+            MessageBox.Show($"First product which is listed on the product class is: {product.ProductName}");
+
+            Product product1 = db.Products.First(x => x.ProductID > 10);
+            MessageBox.Show($" Product which ProductID is bigger than 10:{product1.ProductID} ");
+
+            //Attention!!:If the result will Null this row does not work.That's why we should use try-catch function for preventing application break.
+            try
+            {
+                Category category = db.Categories.First(x => x.CategoryID > 10);
+                MessageBox.Show($"Category which is categoryID bigger than 10:{category.CategoryName}");
+
+            }
+            catch (Exception)
+            {
+
+               MessageBox.Show("Opps..There is no category which categoryID is not bigger than 10 ");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //FirstOrDefault() "This is used for first row same logic like  "First Function Usage".Different of FirstOrDefault is to prevent application breaking and if the result will come "Null" value on the row it is written "null" directly.
+
+            Employee employee = db.Employees.FirstOrDefault(x => x.EmployeeID>12);
+            if (employee==null)
+            {
+                MessageBox.Show("Employee is not found..");
+            }
+            else
+            {
+                MessageBox.Show($"Employee is :{employee.EmployeeID}");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //Find() "As you got from the name of function,this is used to find query from database.In this method using Idendity key is logical because of Id is unique on the sql server database.
+
+            dataGridView1.DataSource = db.Products.Where(x => x.Discontinued == false).OrderBy(x => x.ProductName).ToList();
+            //We listed product which is discontinued order by productname ascendengly.
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //Take () "Take works as a "TOP" function and gives an oppurtunity to print  any row which we wants on the screen.
+
+            dataGridView1.DataSource = db.Categories.Select(x => new
+            {
+               CategoryID= x.CategoryID,
+                CategoryName=x.CategoryName,
+               PictureOfCategories= x.Picture,
+                DescriptionOfCategories=x.Description
+            }).ToList();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //Skip() "In this method, we get skip row that we optioned.
+
+            dataGridView1.DataSource = db.Employees.Select(x => new
+            {
+                x.Address,
+                x.FirstName,
+                x.LastName,
+                x.Region,
+            }).Skip(5).ToList();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //Contains() "It works as a meaning of contains,we get a result from sql and then ".Contains()" we can create a condition.
+            //Attention!!:This method should work with where method.
+
+            dataGridView1.DataSource = db.Customers.Where(x => x.Region.Contains("United States")).OrderBy(x => x.CompanyName).ToList();
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //Any() "This method usa 2 different way:
+            //*Firstly we control  that any record is included  on a List or not?
+            //*Secondly we control  any record that is suitable for our conditions included on a List or not?
+            //As you see,result always Yes or No that means we should use "Boolean Type" for result.
+
+            bool result = db.Customers.Any(x => x.CompanyName == "Technology");
+            MessageBox.Show(result.ToString());
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //Count() "It counts row numbers.The result will be "Integer Type".
+
+            int result = db.Orders.Count();
+            MessageBox.Show($"Total orders: {result}");
+            
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            //Sum() "It used sum all values that we get result.
+
+            int? totalorder = db.Orders.Sum(x => x.Order_Details.Count());
+            MessageBox.Show($"Total Order:{totalorder}");
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+            //DateDiff() " As we know from Sql DateDiff works calculate (What You Want(Year,Month,Day),Start Date,End Date).It means we want to year type End date - Start date.
+            dataGridView1.DataSource = db.Employees.Select(x => new
+            {
+                FirstName = x.FirstName,
+                Lastname = x.LastName,
+                BirthDate = x.BirthDate,
+                Age = SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now)//We use Sql Function that's why we should add SqlFunction to namespace with "Ctrl+."
+            }).ToList();
         }
     }
 }
+
+    
+        
+
+
+
+
+   
+
+
